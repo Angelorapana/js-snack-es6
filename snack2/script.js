1acb2f6422096ee0b40abb6d5d14c1f15239d3f1
angelorapana <angelorapana@icloud.com>
2026-01-11 22:52:33 +0100

    creo ciclo for che prende ogni squadra e assegna numero random (grazie a Math.floor(Math.random()) punti e falli

--- snack2/script.js
+++ snack2/script.js
@@ -1,7 +1,16 @@
+
 const oggetti [
+
     { nome:"juventus", puntiFatti:0, falliSubiti:0,},   
     { nome:"milan", puntiFatti:0, falliSubiti:0,},            
     { nome:"inter", puntiFatti:0, falliSubiti:0,},
     { nome:"napoli", puntiFatti:0, falliSubiti:0,},
     { nome:"roma", puntiFatti:0, falliSubiti:0,},
 ]
+
+//Ciclo for che prende ogni squadra e assegna numero random (grazie a Math.floor(Math.random()) punti e falli
+
+for (let i = 0; i < oggetti.length; i++) { 
+    oggetti[i].puntiFatti = Math.floor(Math.random() * 102); 
+    oggetti[i].falliSubiti = Math.floor(Math.random() * 50); 
+}
