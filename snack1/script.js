61104630ec7fb37b6b4d792b8f0b259bdf4841c1
angelorapana <angelorapana@icloud.com>
2026-01-11 22:26:02 +0100

    Modifico array in array con oggetto letterale

--- snack1/script.js
+++ snack1/script.js
@@ -1,4 +1,12 @@
-//Creazione array
+//Creazione array con oggetto letterale
 
-const oggetti = [ "bicicletta", "quad", "macchina", "moto", "monopattino" "scooter"];
-
+const oggetti = [
+    { nome: "bici", marca: "atala", peso: 10,},   
+    { nome: "bici", marca: "bianchi", peso: 9,},            
+    { nome: "bici", marca: "pinarello", peso: 8,},
+    { nome: "auto", marca: "audi", },
+    { nome: "auto", marca: "bmw", },
+    { nome: "auto", marca: "mercedes",},
+    { nome: "moto", marca: "yamaha", },
+    { nome: "moto", marca: "honda", },
+];  
